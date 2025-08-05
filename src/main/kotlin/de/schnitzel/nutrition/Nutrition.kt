025a94f6504c7dc705eaa6d93ce239052a87ceb2
Iloveschnitzel09 <b.schnitzerling@gmx.de>
2025-08-05 18:27:29 +0200

    Init

---
+++ src/main/kotlin/de/schnitzel/nutrition/Nutrition.kt
@@ -0,0 +1,14 @@
+package de.schnitzel.nutrition
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Nutrition : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
